03da15aa3b100755d4a6f6aa2cc2eef34b25b4e4
Pasha Rezero <kivachuk.2018@gmail.com>
2025-05-03 13:39:19 +0300

    integrate redis and upstash remote database

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ import axios from "axios";
 import qs from "qs";
 import dotenv from "dotenv";
 import cors from "cors";
+import { createClient } from "redis";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 const port = 3000;
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+await redisClient.connect();
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
@@ -45,6 +53,12 @@ const getAccessToken = async () => {
 
 const getPopularTracks = async () => {
   try {
+    const cached = await redisClient.get("popular-tracks");
+
+    if (cached) {
+      return JSON.parse(cached);
+    }
+
     const accessToken = await getAccessToken();
 
     const response = await axios.get(
@@ -55,7 +69,13 @@ const getPopularTracks = async () => {
         },
       }
     );
-    return response.data.tracks.items;
+    const tracks = response.data.tracks.items;
+
+    await redisClient.set("popular-tracks", JSON.stringify(tracks), {
+      EX: 86400,
+    });
+
+    return tracks;
   } catch (error) {
     console.error("Error fetching popular tracks:", error);
   }
